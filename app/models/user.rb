3a76912058caa59c758a25f0495826fad75ad55a
Emmet Susslin & LNM <pair+esusslin.quickresolve@devbootcamp.com>
2016-04-21 19:19:40 -0700

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ActiveRecord::Base
-	validates :username, :email, :password_hash, presence: true
-	validates :email, uniqueness: true
-	has_many :questions
-	has_many :answers
-	has_many :comments
-	has_many :votes
 
 
 	include BCrypt
+
+ 	before_save {self.email.downcase!}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username, :email, :password, presence: true
+  validates :username, length:  { maximum: 12 }
+  validates :email, length:     { maximum: 50 },
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 8 }
+
+  has_many :questions
+	has_many :answers
+	has_many :comments
+	has_many :votes
 
 	def password
 		@password ||= Password.new(password_hash)
@@ -17,5 +25,16 @@ class User < ActiveRecord::Base
 		@password = Password.create(new_password)
 		self.password_hash = @password
 	end
+
+	def self.authenticate(email, password)
+    user_record = User.find_by(email: email)
+    if user_record
+      true if user_record.password == password
+    else
+      false
+    end
+
+  end
+
 
 end

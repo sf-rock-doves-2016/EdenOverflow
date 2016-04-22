77b882379d51e51ed0e473e0033588e19476c17a
QuickResolve <lauranmontoya@gmail.com>
2016-04-21 17:07:31 -0700

    add migration table details

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,3 @@
 class Question < ActiveRecord::Base
-  # Remember to create a migration!
+
 end
--- db/migrate/20160421165119_create_votes.rb
+++ db/migrate/20160421165119_create_votes.rb
@@ -1,4 +1,14 @@
 class CreateVotes < ActiveRecord::Migration
   def change
+    create_table :votes do |t|
+      t.integer :user_id
+      t.integer :votable_id
+      t.string :votable_type
+      t.string :type
+
+      t.timestamps
+    end
+
+    add_index :votes, :votable_id
   end
 end

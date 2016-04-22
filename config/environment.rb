ab0ff74f84b21ad648d97b0b527e48787f1073b3
[01;31m[K"name"[m[K: "Mike London <[01;31m[K"email"[m[K: "mikelawndawn@gmail.com>
2016-04-21 21:57:34 -0700

    Add Initial Seeds/Schwarzefaker

--- config/environment.rb
+++ config/environment.rb
@@ -22,7 +22,7 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 require 'erb'
-
+require 'schwarzefaker'
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 

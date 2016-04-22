2d6de5b1ca33fe3a22fd358a5eac5f8d3ce8504d
PhilB-1 <pbarnett31@gmail.com>
2016-04-22 12:09:23 -0700

    Finalize vote and comment routes and actions.

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,3 +1,11 @@
-post '/answers' do 
-	answer = Answer.create(params[:answer])
+post '/answers' do
+	answer = Answer.new(params[:answer])
+  if answer.save
+    redirect "/questions/#{params[:answer][:question_id]}"
+  else
+    @question = Question.find(params[:answer][:question_id])
+    @author = User.find(@question.user_id)
+    @errors = "Didn't save"
+    erb :'questions/show'
+  end
 end
--- app/controllers/vote.rb
+++ app/controllers/vote.rb
@@ -1,3 +1,9 @@
-post '/votes' do 
-	Vote.create(params[:vote])
+post '/votes' do
+	vote = Vote.create(params[:vote])
+  if vote.votable_type == "Question"
+    redirect "/questions/#{vote.votable_id}"
+  else
+    question = Answer.find(vote.votable_id).question
+    redirect "/questions/#{question.id}"
+  end
 end

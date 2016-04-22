4483b1228c4fc1cb4b3e0cd4705228312e3131c6
Emmet Susslin & LNM <pair+esusslin.quickresolve@devbootcamp.com>
2016-04-21 19:36:30 -0700

    user and session controller complete

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,28 +1,49 @@
-#create new session page
-get '/login' do
-	erb :'/sessions/session_new'
-end
-
-
-#login to session
 post '/sessions' do
-	user = User.find_by(email: params[:email])
-	if user && user.password = params[:password]
-		session[:id] = user.id
-		redirect '/'
-	else
-		# flash[:errors] = ['Incorrect Username or Password']
-		status 400
-		redirect '/login'
-	end
+  access = User.authenticate(params[:email], params[:password])
+  if access
+    puts User.find_by(email: params[:email]).id
+    session[:user_id] = User.find_by(email: params[:email]).id
+    session[:success] = "Successfully Logged In"
+    puts session[:user_id]
+    redirect "/users/#{session[:user_id]}"
+  else
+    session[:errors] = "Email or password do not match records"
+    redirect '/'
+  end
+end
+
+get '/sessions/logout' do
+  session.clear
+  session[:success] = "You have successfully logged out"
+  redirect '/'
 end
 
 
-#delete session
-delete '/sessions/:id' do
-	current_user = nil
-	session[:id] = nil
-	redirect '/templates'
-end
+# #create new session page
+# get '/login' do
+# 	erb :'/sessions/session_new'
+# end
+
+
+# #login to session
+# post '/sessions' do
+# 	user = User.find_by(email: params[:email])
+# 	if user && user.password = params[:password]
+# 		session[:id] = user.id
+# 		redirect '/'
+# 	else
+# 		# flash[:errors] = ['Incorrect Username or Password']
+# 		status 400
+# 		redirect '/login'
+# 	end
+# end
+
+
+# #delete session
+# delete '/sessions/:id' do
+# 	current_user = nil
+# 	session[:id] = nil
+# 	redirect '/templates'
+# end
 
 
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,57 +1,62 @@
-#show all users
-get '/users' do
-	@users = User.all
-	erb :'/users/user_index'
-end
-
-#new user form
 get '/users/new' do
 	erb :'/users/user_new'
 end
 
 #create new user
 post '/users' do
-	user = User.new(username: params[:username], email: params[:email])
-	user.password = params[:password]
-	if user.save
-		session[:id] = user.id
+	new_user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+	if new_user.valid?
+		new_user.save
+		session[:user_id] = new_user.id
+		session[:success] = "Account successfully created"
 		redirect '/'
 	else
-		# status 400
-		# flash[:errors] = user.errors.full_messages
+		session[:errors] = "You need a unique email and password to join Eden Overflow"
 		redirect '/users/new'
 	end
 end
 
 #get edit page
-get '/users/:id/edit' do
-	@user = User.find(params[:id])
-	erb :'/users/user_edit'
-end
+# get '/users/:id/edit' do
+# 	@user = User.find(params[:id])
+# 	erb :'/users/user_edit'
+# end
 
 #show a user
 get '/users/:id' do
-	@user = User.find(params[:id])
+	if session[:user_id] != params[:id].to_i
+    session[:errors] = "You must login to access this page"
+    redirect '/'
+  end
+  @user = User.find(params[:id])
+  if @user
+    erb :'/users/index'
+  else
+    session[:errors] = @user.errors.full_messages[0]
+    redirect '/'
+  end
 end
 
 #submit user edit
-put '/users/:id' do
-	user = User.find(params[:id])
-	user.username = params[:username]
-	user.email = params[:email]
-	if user.save
-		redirect '/'
-	else
-		# flash[:errors] = user.errors.full_messages
-		redirect "/users/#{current_user.id}/edit"
-	end
-end
+# put '/users/:id' do
+# 	user = User.find(params[:id])
+# 	user.username = params[:username]
+# 	user.email = params[:email]
+# 	if user.save
+# 		redirect '/'
+# 	else
+# 		# flash[:errors] = user.errors.full_messages
+# 		redirect "/users/#{current_user.id}/edit"
+# 	end
+# end
 
 #delete user
-delete '/users/:id' do
-	User.find(params[:id]).destroy
-	session[:id] = nil
-	current_user = nil
+delete '/users/:id/delete' do
+  user = User.find(params[:id])
+  user.destroy
+  session.clear
+  redirect '/'
 end
 
 

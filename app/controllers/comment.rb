3531ebd36519f71df9ca27df1da37c0c0289b125
Emmet Susslin & Mike London & LNM <pair+esusslin.mikelondon.quickresolve@devbootcamp.com>
2016-04-22 12:17:29 -0700

    add merge message

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,3 +1,9 @@
 post '/comments' do
-  Comment.create(params[comment])
+  comment = Comment.create(params[:comment])
+  if comment.commentable_type == "Question"
+    redirect "/questions/#{comment.commentable_id}"
+  else
+    question = Answer.find(comment.commentable_id).question
+    redirect "/questions/#{question.id}"
+  end
 end
--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -4,23 +4,6 @@ get '/questions' do
 	erb :'questions/index'
 end
 
-get '/questions/:id' do
-	@question = Question.find(params[:id])
-	@author = User.find(@question.id)
-	erb :'questions/show'
-end
-
-get '/questions/:id/comments/new' do
-  @type = "Question"
-  @id = params[:id]
-  erb :'comments/new'
-end
-
-get '/answers/:id/comments/new' do
-  @type = "Answer"
-  @id = params[:id]
-  erb :'comments/new'
-end
 
 get '/questions/new' do
 	erb :'questions/new'
@@ -34,3 +17,20 @@ post '/questions' do
 	end
 end
 
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @author = User.find(@question.user_id)
+  erb :'questions/show'
+end
+
+get '/questions/:id/comments/new' do
+  @type = "Question"
+  @id = params[:id]
+  erb :'comments/new'
+end
+
+get '/answers/:id/comments/new' do
+  @type = "Answer"
+  @id = params[:id]
+  erb :'comments/new'
+end

ed2b38577030aab4048102a29126ba495adebba9
Emmet Susslin & LNM <pair+esusslin.quickresolve@devbootcamp.com>
2016-04-21 22:13:39 -0700

    add styling and questions display to profile page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'Faker'
 
 20.times.map do
-  User.create!(username: Faker::Name.name, email: Faker::Internet.email, password: "1234")
+  User.create!(username: Faker::Name.name, email: Faker::Internet.email, password: "123456789")
 end
 
 
-User.create(username: "Eric Dell'Aringa", email: "edella2@gmail.com", password: "1234")
+User.create(username: "Eric Dell'Aringa", email: "edella2@gmail.com", password: "123456789")
 
 Question.create(title: "Basic Crud template made for DBC students", body: "Feel free to add to this as you will.  This is a skeleton that you can use on most CRUD apps. There are helper methods written in this as well to make checking which user is currently logged in, if the user should have access to edit/delete a post etc.  Message me on slack or email if you have questions", user_id: 21)
 
